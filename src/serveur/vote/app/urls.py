c450c7b55154380890d2fb0cffce70b6e326c7a2
fabien.burguion <fabien.burguion@epita.fr>
2025-11-14 20:08:29 +0100

    add tests

--- src/serveur/vote/app/urls.py
+++ src/serveur/vote/app/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
 from api.vote_controller import VoteView
 
 urlpatterns = [
